f8d6609c3fe551cdb46f4b25e1d7fe9184510fac
YooJinHa96 <yjs19162@daum.net>
2020-04-24 13:40:29 +0900

    picturbox 생성

--- Mustagram/Mustagram/Form1.cs
+++ Mustagram/Mustagram/Form1.cs
@@ -16,5 +16,10 @@ namespace Mustagram
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            BackColor = Color.White;
+        }
     }
 }
