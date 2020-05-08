8fbb3dd3542d55982938d3a756b9f0861053e7e5
YooJinHa96 <yjs19162@daum.net>
2020-05-08 14:12:56 +0900

    imagel view implementing

--- Mustagram/Mustagram/Form3.Designer.cs
+++ Mustagram/Mustagram/Form3.Designer.cs
@@ -32,8 +32,15 @@
             this.panel1 = new System.Windows.Forms.Panel();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.panel2 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.ciclePicture1 = new Mustagram.CiclePicture();
             this.panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.ciclePicture1)).BeginInit();
             this.SuspendLayout();
             // 
             // panel1
@@ -50,7 +57,7 @@
             // pictureBox1
             // 
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(160, 0);
+            this.pictureBox1.Location = new System.Drawing.Point(169, 0);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(121, 60);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
@@ -60,11 +67,68 @@
             // panel2
             // 
             this.panel2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel2.Controls.Add(this.label2);
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Controls.Add(this.ciclePicture1);
             this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
             this.panel2.Location = new System.Drawing.Point(0, 62);
             this.panel2.Name = "panel2";
             this.panel2.Size = new System.Drawing.Size(475, 59);
             this.panel2.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Comic Sans MS", 11F);
+            this.label1.Location = new System.Drawing.Point(76, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(69, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Seo_jisu";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("굴림", 5F);
+            this.label2.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label2.Location = new System.Drawing.Point(440, 25);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(25, 7);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "●●●";
+            // 
+            // listView1
+            // 
+            this.listView1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.listView1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(0, 121);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(475, 479);
+            this.listView1.TabIndex = 2;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // panel3
+            // 
+            this.panel3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel3.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.panel3.Location = new System.Drawing.Point(0, 600);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(475, 52);
+            this.panel3.TabIndex = 3;
+            // 
+            // ciclePicture1
+            // 
+            this.ciclePicture1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.ciclePicture1.Image = ((System.Drawing.Image)(resources.GetObject("ciclePicture1.Image")));
+            this.ciclePicture1.Location = new System.Drawing.Point(12, 5);
+            this.ciclePicture1.Name = "ciclePicture1";
+            this.ciclePicture1.Size = new System.Drawing.Size(49, 49);
+            this.ciclePicture1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.ciclePicture1.TabIndex = 0;
+            this.ciclePicture1.TabStop = false;
             // 
             // Form3
             // 
@@ -72,14 +136,20 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
             this.ClientSize = new System.Drawing.Size(475, 652);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.listView1);
             this.Controls.Add(this.panel2);
             this.Controls.Add(this.panel1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "Form3";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form3";
+            this.Load += new System.EventHandler(this.Form3_Load);
             this.panel1.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.ciclePicture1)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -89,5 +159,10 @@
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.Panel panel2;
+        private CiclePicture ciclePicture1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.Panel panel3;
     }
 }
--- Mustagram/Mustagram/Form3.cs
+++ Mustagram/Mustagram/Form3.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Text;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -16,5 +18,44 @@ namespace Mustagram
         {
             InitializeComponent();
         }
+
+        private void Form3_Load(object sender, EventArgs e)
+        {
+            listView1.View = View.Details;
+
+            listView1.Columns.Add("Spacecraft", 300); ;
+            listView1.AutoResizeColumn(0, ColumnHeaderAutoResizeStyle.HeaderSize);
+            populate();
+
+        }
+        private void populate()
+        {
+            ImageList imgs = new ImageList();
+            imgs.ImageSize = new Size(50, 50);
+         
+            //Load IMGS FROM FILE
+            String[] paths = { };
+            paths = Directory.GetFiles("C:/Users/poop4/Documents/GitHub/Mustagram/Mustagram/Mustagram/imagetest");//server host? 넣기
+
+            try
+            {
+                foreach (String path in paths)
+                {
+                    imgs.Images.Add(Image.FromFile(path));
+                }
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message);
+            }
+
+            //BIND IMGS TO LISTVIEW
+            listView1.SmallImageList = imgs;
+            listView1.Items.Add("jusu", 0);
+            listView1.Items.Add("jusu2", 1);
+
+        }
+
+      
     }
 }
